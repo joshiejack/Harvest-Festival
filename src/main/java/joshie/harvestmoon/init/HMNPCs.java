82ca0e0c84e6f22ef9341c29cd75f82c3cc53f3b
joshiejack <joshjackwildman@gmail.com>
2015-02-16 12:01:43 +0000

    Redistribute birthdays.

--- src/main/java/joshie/harvestmoon/init/HMNPCs.java
+++ src/main/java/joshie/harvestmoon/init/HMNPCs.java
@@ -51,22 +51,22 @@ public class HMNPCs {
 
     public static void init() {
         goddess = register(new NPCGoddess("goddess", FEMALE, ADULT));
-        animal_owner = register(new NPC("jim", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        cafe_owner = register(new NPC("liara", FEMALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        seed_owner = register(new NPC("jade", FEMALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        tool_owner = register(new NPC("daniel", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        priest = register(new NPC("thomas", MALE, ELDER, new CalendarDate(1, Season.SPRING, 1)));
-        seed_child = register(new NPC("fenn", MALE, CHILD, new CalendarDate(1, Season.SPRING, 1)));
-        cafe_granny = register(new NPC("katlin", FEMALE, ELDER, new CalendarDate(1, Season.SPRING, 1)));
-        mayor = register(new NPC("jamie", FEMALE, ELDER, new CalendarDate(1, Season.SPRING, 1)));
-        daughter_1 = register(new NPC("cloe", FEMALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        daughter_2 = register(new NPC("abi", FEMALE, CHILD, new CalendarDate(1, Season.SPRING, 1)));
-        clock_worker = register(new NPC("tiberius", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        gs_owner = register(new NPC("jenni", FEMALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        miner = register(new NPC("brandon", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)).setIsMiner());
-        fisherman = register(new NPC("jacob", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        milkmaid = register(new NPC("candice", FEMALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
-        builder = register(new NPC("inap", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)).setIsBuilder());
-        poultry = register(new NPC("ondra", MALE, ADULT, new CalendarDate(1, Season.SPRING, 1)));
+        animal_owner = register(new NPC("jim", MALE, ADULT, new CalendarDate(26, Season.SPRING, 1)));
+        cafe_owner = register(new NPC("liara", FEMALE, ADULT, new CalendarDate(17, Season.SPRING, 1)));
+        seed_owner = register(new NPC("jade", FEMALE, ADULT, new CalendarDate(14, Season.SPRING, 1)));
+        daughter_1 = register(new NPC("cloe", FEMALE, ADULT, new CalendarDate(3, Season.SPRING, 1)));
+        priest = register(new NPC("thomas", MALE, ELDER, new CalendarDate(9, Season.SUMMER, 1)));
+        seed_child = register(new NPC("fenn", MALE, CHILD, new CalendarDate(25, Season.SUMMER, 1)));
+        cafe_granny = register(new NPC("katlin", FEMALE, ELDER, new CalendarDate(12, Season.SUMMER, 1)));
+        mayor = register(new NPC("jamie", FEMALE, ELDER, new CalendarDate(8, Season.SUMMER, 1)));
+        builder = register(new NPC("inap", MALE, ADULT, new CalendarDate(19, Season.SUMMER, 1)).setIsBuilder());
+        tool_owner = register(new NPC("daniel", MALE, ADULT, new CalendarDate(1, Season.WINTER, 1)));
+        daughter_2 = register(new NPC("abi", FEMALE, CHILD, new CalendarDate(27, Season.WINTER, 1)));
+        clock_worker = register(new NPC("tiberius", MALE, ADULT, new CalendarDate(15, Season.WINTER, 1)));
+        gs_owner = register(new NPC("jenni", FEMALE, ADULT, new CalendarDate(7, Season.WINTER, 1)));
+        miner = register(new NPC("brandon", MALE, ADULT, new CalendarDate(13, Season.AUTUMN, 1)).setIsMiner());
+        fisherman = register(new NPC("jacob", MALE, ADULT, new CalendarDate(28, Season.AUTUMN, 1)));
+        milkmaid = register(new NPC("candice", FEMALE, ADULT, new CalendarDate(5, Season.AUTUMN, 1)));
+        poultry = register(new NPC("ondra", MALE, ADULT, new CalendarDate(16, Season.AUTUMN, 1)));
     }
 }

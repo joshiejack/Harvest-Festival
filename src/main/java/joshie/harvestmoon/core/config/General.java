01344928ff79da341f0c7e9ba171063ee32c5602
joshiejack <joshjackwildman@gmail.com>
2015-02-25 12:23:43 +0000

    The Onion.

--- src/main/java/joshie/harvestmoon/core/config/General.java
+++ src/main/java/joshie/harvestmoon/core/config/General.java
@@ -3,7 +3,7 @@ import static joshie.harvestmoon.core.helpers.generic.ConfigHelper.getBoolean;
 import net.minecraftforge.common.config.Configuration;
 
 public class General {
-    public static boolean DEBUG_MODE = true;
+    public static boolean DEBUG_MODE = false;
     
     public static void init (Configuration config) {
         //DEBUG_MODE = getBoolean("Enable Debug Mode", false);
--- src/main/java/joshie/harvestmoon/init/HMRecipeFixes.java
+++ src/main/java/joshie/harvestmoon/init/HMRecipeFixes.java
@@ -13,6 +13,7 @@ import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class HMRecipeFixes {
     public static void init() {
@@ -30,21 +31,50 @@ public class HMRecipeFixes {
                 overrideShapeless((ShapelessRecipes) recipe, fix);
             } else if (recipe instanceof ShapedOreRecipe) {
                 overrideShapedOre((ShapedOreRecipe) recipe, fix);
+            } else if (recipe instanceof ShapelessOreRecipe) {
+                overrideShapelessOre((ShapelessOreRecipe) recipe, fix);
             }
+        }
+    }
+
+    private static void overrideShapelessOre(ShapelessOreRecipe recipe, Item fix) {
+        ArrayList<Object> original = recipe.getInput();
+        ArrayList<Object> clone = new ArrayList(original.size());
+        boolean hasChanged = false;
+        for (int i = 0; i < original.size(); i++) {
+            Object o = original.get(i);
+            if (o == null || !(o instanceof ItemStack)) {
+                clone.add(o);
+                continue;
+            }
+
+            ItemStack stack = (ItemStack) o;
+            Item item = stack.getItem();
+            if (item == fix) {
+                ItemStack add = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
+                add.setTagCompound(stack.getTagCompound());
+                clone.add(add);
+                hasChanged = true;
+            } else clone.add(stack.copy());
+        }
+        
+        if (hasChanged) {
+            override(recipe, clone);
         }
     }
 
     private static void overrideShapedOre(ShapedOreRecipe recipe, Item fix) {
         Object[] original = recipe.getInput();
         Object[] clone = new Object[original.length];
+        boolean hasChanged = false;
+
         for (int i = 0; i < original.length; i++) {
-            boolean hasChanged = false;
             Object o = original[i];
             if (o == null || !(o instanceof ItemStack)) {
                 clone[i] = o;
                 continue;
             }
-            
+
             ItemStack stack = (ItemStack) o;
             Item item = stack.getItem();
             if (item == fix) {
@@ -52,25 +82,26 @@ public class HMRecipeFixes {
                 ((ItemStack) clone[i]).setTagCompound(stack.getTagCompound());
                 hasChanged = true;
             } else clone[i] = ((ItemStack) original[i]).copy();
-
-            if (hasChanged) {
-                override(recipe, clone);
-            }
+        }
+        
+        if (hasChanged) {
+            override(recipe, clone);
         }
     }
 
     private static void overrideShapeless(ShapelessRecipes recipe, Item fix) {
         ArrayList<ItemStack> original = (ArrayList<ItemStack>) recipe.recipeItems;
         ArrayList<ItemStack> clone = new ArrayList(original.size());
-        while (clone.size() < original.size()) clone.add(null);
-        for (int i = 0; i < original.size(); i++) {
-            boolean hasChanged = false;
+        boolean hasChanged = false;
+        while (clone.size() < original.size())
+            clone.add(null);
+        for (int i = 0; i < original.size(); i++) {            
             ItemStack stack = original.get(i);
             if (stack == null) {
                 clone.set(i, null);
                 continue;
             }
-            
+
             Item item = stack.getItem();
             if (item == fix) {
                 ItemStack stack2 = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
@@ -78,34 +109,34 @@ public class HMRecipeFixes {
                 clone.set(i, stack2);
                 hasChanged = true;
             } else clone.set(i, stack);
-
-            if (hasChanged) {
-                override(recipe, clone);
-            }
+        }
+        
+        if (hasChanged) {
+            override(recipe, clone);
         }
     }
 
     private static void overrideShaped(ShapedRecipes shaped, Item fix) {
         ItemStack[] original = shaped.recipeItems;
         ItemStack[] clone = new ItemStack[original.length];
+        boolean hasChanged = false;
         for (int i = 0; i < original.length; i++) {
-            boolean hasChanged = false;
             ItemStack stack = original[i];
             if (stack == null) {
                 clone[i] = null;
                 continue;
             }
-            
+
             Item item = stack.getItem();
             if (item == fix) {
                 clone[i] = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
                 clone[i].setTagCompound(stack.getTagCompound());
                 hasChanged = true;
             } else clone[i] = original[i].copy();
-
-            if (hasChanged) {
-                override(shaped, clone);
-            }
+        }
+        
+        if (hasChanged) {
+            override(shaped, clone);
         }
     }
 
@@ -114,6 +145,7 @@ public class HMRecipeFixes {
             Field field = recipe.getClass().getDeclaredField("recipeItems");
             if (field == null) field = recipe.getClass().getDeclaredField("field_77574_d");
             if (field == null) field = recipe.getClass().getDeclaredField("field_77579_b");
+            if (field == null) field = recipe.getClass().getDeclaredField("input");
             Field modifiersField = Field.class.getDeclaredField("modifiers");
             modifiersField.setAccessible(true);
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);

5698c86c13a184d8763c4905654ec1a583537ffa
joshiejack <joshjackwildman@gmail.com>
2015-02-26 14:58:31 +0000

    Replace Items.carrot with cropCarrot etc. Where appropriate.

--- src/main/java/joshie/harvestmoon/init/HMRecipeFixes.java
+++ src/main/java/joshie/harvestmoon/init/HMRecipeFixes.java
@@ -17,27 +17,27 @@ import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class HMRecipeFixes {
     public static void init() {
-        fixRecipes(Items.egg);
-        fixRecipes(Items.carrot);
-        fixRecipes(Items.potato);
-        fixRecipes(Items.wheat);
+        fixRecipes(Items.egg, null);
+        fixRecipes(Items.carrot, "Carrot");
+        fixRecipes(Items.potato, "Potato");
+        fixRecipes(Items.wheat, "Wheat");
     }
 
-    public static void fixRecipes(Item fix) {
+    public static void fixRecipes(Item fix, String name) {
         for (IRecipe recipe : (ArrayList<IRecipe>) CraftingManager.getInstance().getRecipeList()) {
             if (recipe instanceof ShapedRecipes) {
                 overrideShaped((ShapedRecipes) recipe, fix);
             } else if (recipe instanceof ShapelessRecipes) {
                 overrideShapeless((ShapelessRecipes) recipe, fix);
             } else if (recipe instanceof ShapedOreRecipe) {
-                overrideShapedOre((ShapedOreRecipe) recipe, fix);
+                overrideShapedOre((ShapedOreRecipe) recipe, fix, name);
             } else if (recipe instanceof ShapelessOreRecipe) {
-                overrideShapelessOre((ShapelessOreRecipe) recipe, fix);
+                overrideShapelessOre((ShapelessOreRecipe) recipe, fix, name);
             }
         }
     }
 
-    private static void overrideShapelessOre(ShapelessOreRecipe recipe, Item fix) {
+    private static void overrideShapelessOre(ShapelessOreRecipe recipe, Item fix, String name) {
         ArrayList<Object> original = recipe.getInput();
         ArrayList<Object> clone = new ArrayList(original.size());
         boolean hasChanged = false;
@@ -51,19 +51,23 @@ public class HMRecipeFixes {
             ItemStack stack = (ItemStack) o;
             Item item = stack.getItem();
             if (item == fix) {
-                ItemStack add = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
-                add.setTagCompound(stack.getTagCompound());
+                Object add = null;
+                if (name == null) {
+                    add = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
+                    ((ItemStack) add).setTagCompound(stack.getTagCompound());
+                } else add = "crop" + name;
+
                 clone.add(add);
                 hasChanged = true;
             } else clone.add(stack.copy());
         }
-        
+
         if (hasChanged) {
             override(recipe, clone);
         }
     }
 
-    private static void overrideShapedOre(ShapedOreRecipe recipe, Item fix) {
+    private static void overrideShapedOre(ShapedOreRecipe recipe, Item fix, String name) {
         Object[] original = recipe.getInput();
         Object[] clone = new Object[original.length];
         boolean hasChanged = false;
@@ -78,12 +82,16 @@ public class HMRecipeFixes {
             ItemStack stack = (ItemStack) o;
             Item item = stack.getItem();
             if (item == fix) {
-                clone[i] = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
-                ((ItemStack) clone[i]).setTagCompound(stack.getTagCompound());
+                clone[i] = null;
+                if (name == null) {
+                    clone[i] = new ItemStack(fix, 1, OreDictionary.WILDCARD_VALUE);
+                    ((ItemStack) clone[i]).setTagCompound(stack.getTagCompound());
+                } else clone[i] = "crop" + name;
+
                 hasChanged = true;
             } else clone[i] = ((ItemStack) original[i]).copy();
         }
-        
+
         if (hasChanged) {
             override(recipe, clone);
         }
@@ -95,7 +103,7 @@ public class HMRecipeFixes {
         boolean hasChanged = false;
         while (clone.size() < original.size())
             clone.add(null);
-        for (int i = 0; i < original.size(); i++) {            
+        for (int i = 0; i < original.size(); i++) {
             ItemStack stack = original.get(i);
             if (stack == null) {
                 clone.set(i, null);
@@ -110,7 +118,7 @@ public class HMRecipeFixes {
                 hasChanged = true;
             } else clone.set(i, stack);
         }
-        
+
         if (hasChanged) {
             override(recipe, clone);
         }
@@ -134,7 +142,7 @@ public class HMRecipeFixes {
                 hasChanged = true;
             } else clone[i] = original[i].copy();
         }
-        
+
         if (hasChanged) {
             override(shaped, clone);
         }

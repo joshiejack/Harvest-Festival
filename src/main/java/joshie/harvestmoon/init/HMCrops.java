b64c258a223b0e8f6c36c849f80dfb86ecf8a6ce
Yulife <YulifeGaming@t-online.de>
2015-05-14 23:47:13 +0200

    Colors are now correct

--- src/main/java/joshie/harvestmoon/init/HMCrops.java
+++ src/main/java/joshie/harvestmoon/init/HMCrops.java
@@ -58,30 +58,30 @@ public class HMCrops {
         null_crop = HMApi.CROPS.registerCrop(new CropNull().setCropIconHandler(new IconHandlerNull()));
 
         //Spring Crops
-        turnip = HMApi.CROPS.registerCrop("turnip", 120, 60, 5, 0, 0, 0xEDE1B5, SPRING).setCropIconHandler(new IconHandlerTurnip());
-        potato = HMApi.CROPS.registerCrop("potato", 150, 80, 8, 0, 0, 0x8D782A, SPRING).setDropHandler(new DropPotato()).setCropIconHandler(new IconHandlerSeedFood(Blocks.potatoes));
-        cucumber = HMApi.CROPS.registerCrop("cucumber", 200, 60, 10, 5, 0, 0x137B27, SPRING).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerCucumber());
+        turnip = HMApi.CROPS.registerCrop("turnip", 120, 60, 5, 0, 0, 0xffffff, SPRING).setCropIconHandler(new IconHandlerTurnip());
+        potato = HMApi.CROPS.registerCrop("potato", 150, 80, 8, 0, 0, 0xbe8d2b, SPRING).setDropHandler(new DropPotato()).setCropIconHandler(new IconHandlerSeedFood(Blocks.potatoes));
+        cucumber = HMApi.CROPS.registerCrop("cucumber", 200, 60, 10, 5, 0, 0x36b313, SPRING).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerCucumber());
         strawberry = HMApi.CROPS.registerCrop("strawberry", 150, 30, 9, 7, 3, 0xff7bea, SPRING).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerStrawberry());
-        cabbage = HMApi.CROPS.registerCrop("cabbage", 500, 250, 15, 0, 8, 0x8DF146, SPRING).setCropIconHandler(new IconHandlerCabbage());
+        cabbage = HMApi.CROPS.registerCrop("cabbage", 500, 250, 15, 0, 8, 0x8fff40, SPRING).setCropIconHandler(new IconHandlerCabbage());
 
         //Summer Crops
-        tomato = HMApi.CROPS.registerCrop("tomato", 200, 60, 10, 7, 0, 0xF23B0C, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerTomato());
-        onion = HMApi.CROPS.registerCrop("onion", 150, 80, 8, 0, 0, 0xF3B073, SUMMER).setCropIconHandler(new IconHandlerOnion());
-        corn = HMApi.CROPS.registerCrop("corn", 300, 100, 15, 12, 0, 0xF8E048, SUMMER).setCropIconHandler(new IconHandlerCorn());
-        pumpkin = HMApi.CROPS.registerCrop("pumpkin", 500, 125, 15, 0, 3, 0x54971E, SUMMER).setGrowsToSide(Blocks.pumpkin).setCropIconHandler(new IconHandlerBlock(Blocks.pumpkin));
-        pineapple = HMApi.CROPS.registerCrop("pineapple", 1000, 500, 21, 5, 8, 0xEECD33, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerPineapple());
+        onion = HMApi.CROPS.registerCrop("onion", 150, 80, 8, 0, 0, 0xdcc307, SUMMER).setCropIconHandler(new IconHandlerOnion());
+        tomato = HMApi.CROPS.registerCrop("tomato", 200, 60, 10, 7, 0, 0xe60820, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerTomato());
+        corn = HMApi.CROPS.registerCrop("corn", 300, 100, 15, 12, 0, 0xd4bd45, SUMMER).setCropIconHandler(new IconHandlerCorn());
+        pumpkin = HMApi.CROPS.registerCrop("pumpkin", 500, 125, 15, 0, 3, 0xe09a39, SUMMER).setGrowsToSide(Blocks.pumpkin).setCropIconHandler(new IconHandlerBlock(Blocks.pumpkin));
+        pineapple = HMApi.CROPS.registerCrop("pineapple", 1000, 500, 21, 5, 8, 0xd7cf00, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setCropIconHandler(new IconHandlerPineapple());
 
         //Autumn Crops
-        eggplant = HMApi.CROPS.registerCrop("eggplant", 120, 80, 10, 7, 0, 0x9F61C8, AUTUMN).setCropIconHandler(new IconHandlerEggplant());
-        carrot = HMApi.CROPS.registerCrop("carrot", 300, 120, 8, 0, 0, 0xF79316, AUTUMN).setCropIconHandler(new IconHandlerSeedFood(Blocks.carrots));
-        sweet_potato = HMApi.CROPS.registerCrop("potato_sweet", 300, 120, 6, 4, 0, 0xCD1A8B, AUTUMN).setCropIconHandler(new IconHandlerSweetPotato());
-        spinach = HMApi.CROPS.registerCrop("spinach", 200, 80, 6, 0, 3, 0x30B028, AUTUMN).setCropIconHandler(new IconHandlerSpinach());
-        green_pepper = HMApi.CROPS.registerCrop("pepper_green", 150, 40, 8, 2, 8, 0x1F5F12, AUTUMN).setCropIconHandler(new IconHandlerGreenPepper());
+        eggplant = HMApi.CROPS.registerCrop("eggplant", 120, 80, 10, 7, 0, 0xa25cc4, AUTUMN).setCropIconHandler(new IconHandlerEggplant());
+        spinach = HMApi.CROPS.registerCrop("spinach", 200, 80, 6, 0, 3, 0x90ae15, AUTUMN).setCropIconHandler(new IconHandlerSpinach());
+        carrot = HMApi.CROPS.registerCrop("carrot", 300, 120, 8, 0, 0, 0xf8ac33, AUTUMN).setCropIconHandler(new IconHandlerSeedFood(Blocks.carrots));
+        sweet_potato = HMApi.CROPS.registerCrop("potato_sweet", 300, 120, 6, 4, 0, 0xd82aac, AUTUMN).setCropIconHandler(new IconHandlerSweetPotato());
+        green_pepper = HMApi.CROPS.registerCrop("pepper_green", 150, 40, 8, 2, 8, 0x56d213, AUTUMN).setCropIconHandler(new IconHandlerGreenPepper());
 
         //All Seasons
-        grass = HMApi.CROPS.registerCrop("grass", 500, 0, 11, 0, 0, 0x006633, SPRING, SUMMER, AUTUMN).setAnimalFoodType(AnimalFoodType.GRASS).setBecomesDouble(6).setIsStatic().setHasAlternativeName().setRequiresSickle().setCropIconHandler(new IconHandlerGrass());
-        wheat = HMApi.CROPS.registerCrop("wheat", 150, 100, 28, 0, 0, 0x8C8C00, SPRING, SUMMER, AUTUMN).setAnimalFoodType(AnimalFoodType.GRASS).setRequiresSickle().setCropIconHandler(new IconHandlerWheat());
-        watermelon = HMApi.CROPS.registerCrop("watermelon", 250, 25, 11, 0, 3, 0xFF73FF, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setDropHandler(new DropMelon()).setGrowsToSide(Blocks.melon_block).setCropIconHandler(new IconHandlerBlock(Blocks.melon_block));
+        grass = HMApi.CROPS.registerCrop("grass", 500, 0, 11, 0, 0, 0x7ac958, SPRING, SUMMER, AUTUMN).setAnimalFoodType(AnimalFoodType.GRASS).setBecomesDouble(6).setIsStatic().setHasAlternativeName().setRequiresSickle().setCropIconHandler(new IconHandlerGrass());
+        wheat = HMApi.CROPS.registerCrop("wheat", 150, 100, 28, 0, 0, 0xeac715, SPRING, SUMMER, AUTUMN).setAnimalFoodType(AnimalFoodType.GRASS).setRequiresSickle().setCropIconHandler(new IconHandlerWheat());
+        watermelon = HMApi.CROPS.registerCrop("watermelon", 250, 25, 11, 0, 3, 0xff3211, SUMMER).setAnimalFoodType(AnimalFoodType.FRUIT).setDropHandler(new DropMelon()).setGrowsToSide(Blocks.melon_block).setCropIconHandler(new IconHandlerBlock(Blocks.melon_block));
         if (HMConfiguration.vanilla.POTATO_OVERRIDE) potato.setItem(Items.potato);
         if (HMConfiguration.vanilla.CARROT_OVERRIDE) carrot.setItem(Items.carrot);
         if (HMConfiguration.vanilla.WHEAT_OVERRIDE) wheat.setItem(Items.wheat);

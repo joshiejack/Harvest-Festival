9b463e7d6b6ee2a7907ce690a5be0bfec85c1b5e
Yulife <YulifeGaming@t-online.de>
2015-08-29 11:38:11 +0200

    Screw this
    @joshiejack why is the stone crashing q_q

--- src/main/java/joshie/harvest/core/HFTab.java
+++ src/main/java/joshie/harvest/core/HFTab.java
@@ -98,7 +98,9 @@ public class HFTab extends CreativeTabs {
 
     public static void preInit() {
         HFTab.tabTown.setStack(new ItemStack(HFItems.structures, 0, 0));
-        HFTab.tabMining.setStack(new ItemStack(HFBlocks.stone));
+        /*
+        HFTab.tabMining.setStack(new ItemStack(HFBlocks.stone, 0, 0));
+        */
         HFTab.tabFarming.setStack(new ItemStack(HFItems.hoe, 1, 0));
         HFTab.tabCooking.setStack(HFApi.COOKING.getMeal("salad"));
     }

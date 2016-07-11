ab332f3c1166eb4957e1e03de04d3f5c7e821543
joshiejack <joshjackwildman@gmail.com>
2016-07-11 02:38:34 +0100

    Jakob doesn't have a shop stop trying to check it

--- src/main/java/joshie/harvest/npc/schedule/ScheduleJacob.java
+++ src/main/java/joshie/harvest/npc/schedule/ScheduleJacob.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.world.World;
 
 import static joshie.harvest.api.npc.INPC.Location.HOME;
-import static joshie.harvest.api.npc.INPC.Location.WORK;
 import static joshie.harvest.town.TownData.FISHING_POND;
 
 public class ScheduleJacob implements ISchedule {
@@ -16,7 +15,6 @@ public class ScheduleJacob implements ISchedule {
 
     @Override
     public BuildingLocation getTarget(World world, EntityLiving entity, INPC npc, long time) {
-        if (npc.getShop() != null && npc.getShop().isOpen(world, null)) return npc.getLocation(WORK);
         if (time >= 5000L && time <= 9000L) return new BuildingLocation(HFBuildings.FISHING_HOLE, FISHING_POND);
         else return npc.getLocation(HOME);
     }

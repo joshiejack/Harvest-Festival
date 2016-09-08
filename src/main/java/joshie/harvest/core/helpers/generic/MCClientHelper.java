a28daeaa564f953bdfc02b4e99c7cad5ebed7833
joshiejack <joshjackwildman@gmail.com>
2016-09-08 23:42:10 +0100

    Remove side only

--- src/main/java/joshie/harvest/core/helpers/generic/MCClientHelper.java
+++ src/main/java/joshie/harvest/core/helpers/generic/MCClientHelper.java
@@ -11,13 +11,10 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.FMLClientHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.input.Mouse;
 
 import java.awt.*;
 
-@SideOnly(Side.CLIENT)
 public class MCClientHelper {
     public static Minecraft getMinecraft() {
         return FMLClientHandler.instance().getClient();

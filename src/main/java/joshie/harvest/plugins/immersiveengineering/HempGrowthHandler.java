1b984285c6349f56b0cd87be0d590ed8746a6661
joshiejack <joshjackwildman@gmail.com>
2016-10-01 07:15:55 +0100

    Experimental IE Hemp.

---
+++ src/main/java/joshie/harvest/plugins/immersiveengineering/HempGrowthHandler.java
@@ -0,0 +1,4 @@
+package joshie.harvest.plugins.immersiveengineering;
+
+public class HempGrowthHandler extends joshie.harvest.api.crops.GrowthHandler {
+}
---
+++ src/main/java/joshie/harvest/plugins/immersiveengineering/ImmersiveEngineering.java
@@ -0,0 +1,4 @@
+package joshie.harvest.plugins.immersiveengineering;
+
+public class ImmersiveEngineering {
+}

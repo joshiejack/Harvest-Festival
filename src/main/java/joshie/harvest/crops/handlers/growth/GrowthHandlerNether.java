d768bca85a29c60891925b22163e9c6eb475d884
joshiejack <joshjackwildman@gmail.com>
2016-12-03 09:05:10 +0000

    Missed one.

--- src/main/java/joshie/harvest/crops/handlers/growth/GrowthHandlerNether.java
+++ src/main/java/joshie/harvest/crops/handlers/growth/GrowthHandlerNether.java
@@ -2,13 +2,9 @@ package joshie.harvest.crops.handlers.growth;
 
 import joshie.harvest.api.crops.Crop;
 import joshie.harvest.api.crops.GrowthHandler;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -20,13 +16,7 @@ public class GrowthHandlerNether extends GrowthHandler<Crop> {
     public void addInformation(List<String> list, Crop crop, boolean debug) {
         list.add(TextFormatting.RED + "Nether");
     }
-
-    @Override
-    @Deprecated
-    public boolean canSustainCrop(IBlockAccess world, BlockPos pos, IBlockState state, Crop crop) {
-        return state.getBlock() == Blocks.SOUL_SAND && crop.getPlantType() == EnumPlantType.Nether;
-    }
-
+    
     @Override
     public boolean canGrow(World world, BlockPos pos, Crop crop) {
         return world.provider.getDimension() == -1;
